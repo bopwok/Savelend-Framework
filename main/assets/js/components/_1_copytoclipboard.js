fcfda8a7aec8348131f6757f8ccd5d94493cf935
SavelendMartin <martin@savelend.se>
2021-05-26 11:05:35 +0200

    added copy to clipboard

--- main/assets/js/components/_1_copytoclipboard.js
+++ main/assets/js/components/_1_copytoclipboard.js
@@ -1,11 +1,28 @@
-const copyToClipboard = str => {
-  const el = document.createElement('textarea');
-  el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
+function showFlashMessage(element) {
+  var event = new CustomEvent('showFlashMessage');
+  element.dispatchEvent(event);
 };
+
+var flashMessage = document.getElementById('copyMessage');
+
+const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
+
+var copyItems = document.getElementsByClassName("copytoclipboard");
+if (copyItems) {
+    for (let copyItem of copyItems) {
+        copyItem.addEventListener('click', function() {
+            copyToClipboard(copyItem.dataset.copy);
+            showFlashMessage(flashMessage);
+        });
+    };
+}
